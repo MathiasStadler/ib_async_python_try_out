ef2682f80261e94759c37676cb9987b5a0cd1f98
Mathias Stadler <stadler-mathias@web.de>
2025-04-11 10:36:57 +0200

    save

---
+++ src/python_logging_file_1.py
@@ -0,0 +1,25 @@
+# reason outside jupyter notebook
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    # logging.basicConfig(filename='myapp.log', level=logging.INFO)
+    logging.basicConfig(filename='myapp1.log',
+                        level=logging.DEBUG,
+                        format="[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    logging.debug("This is a debug message")
+    logging.info("This is an info message")
+    logging.warning("This is a warning message")
+    logging.error("This is an error message")
+    logging.critical("This is a critical message")
+
+    logger.info('Started')
+    logger.info('working')
+    logger.info('Finished')
+
+
+if __name__ == '__main__':
+    main()
